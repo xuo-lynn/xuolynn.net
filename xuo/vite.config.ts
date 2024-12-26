000e5a3c98c9d4113bccc994f664b7588aec9e66
xuo <61861557+xuo-lynn@users.noreply.github.com>
2024-12-25 20:43:51 -0500

    Update vite.config.ts

--- xuo/vite.config.ts
+++ xuo/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vite.dev/configs
 export default defineConfig({
   plugins: [react()],
 })
