2a94ffe80a2bbad0ba268ecd9245f9b896f3bed9
xuo <61861557+xuo-lynn@users.noreply.github.com>
2024-12-28 14:02:53 -0500

    Update Projects.tsx

--- xuo/src/Pages/Projects.tsx
+++ xuo/src/Pages/Projects.tsx
@@ -42,7 +42,7 @@ const Projects = () => {
             />
             <div className="relative">
               <Cinamoroll />
-              <p className="absolute inset-0 flex flex-col justify-center items-center pt-[150px] text-lg text-gray-400 text-center mt-2">
+              <p className="absolute inset-0 flex flex-col justify-center items-center pt-[160px] text-lg text-gray-400 text-center mt-2">
                 Thanks for visiting!<br/>
                 Built and designed by xuo ©
               </p>
