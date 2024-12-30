d7931e04f7c559c5466b7a665f4171f2d8ee81d5
xuo <61861557+xuo-lynn@users.noreply.github.com>
2024-12-30 07:50:29 -0500

    analytics redo

--- xuo/src/App.tsx
+++ xuo/src/App.tsx
@@ -1,10 +1,11 @@
 import Hero from './Pages/Hero.tsx';
 import Projects from './Pages/Projects.tsx';
 import Discord from './Pages/Discord.tsx';
-
+import { Analytics } from '@vercel/analytics/react'
 function App() {
   return (
     <div className="flex flex-col">
+      <Analytics />
       <Hero />
       <Discord /> 
       <Projects />
