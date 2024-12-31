315731cb9b6f656422379f3f39446d49fdf4ae3e
xuo <61861557+xuo-lynn@users.noreply.github.com>
2024-12-30 19:29:47 -0500

    Update Lanyard.tsx

--- xuo/src/Components/Lanyard.tsx
+++ xuo/src/Components/Lanyard.tsx
@@ -168,7 +168,7 @@ const Lanyard: React.FC = () => {
             const b = pixels[i + 2];
 
             // filter out black and white ranges 
-            if (!(r < 60 && g < 60 && b < 60) && !(r > 190 && g > 190 && b > 190)) {
+            if (!(r < 50 && g < 50 && b < 70) && !(r > 200 && g > 200 && b > 200)) {
               const color = `${r},${g},${b}`;
               colorCount[color] = (colorCount[color] || 0) + 1;
 
