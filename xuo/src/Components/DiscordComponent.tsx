e6d57e44c96bf80fee98d17634047da9643209bd
xuo <61861557+xuo-lynn@users.noreply.github.com>
2025-01-06 12:01:25 -0500

    Update DiscordComponent.tsx

--- xuo/src/Components/DiscordComponent.tsx
+++ xuo/src/Components/DiscordComponent.tsx
@@ -133,7 +133,7 @@ const DiscordComponent: React.FC = () => {
                                     {activeSection === 'about' && (
                                         <div>
                                             <p className="text-slate-300"> 
-                                                I'm a junior software engineer with previous back-end experience at <strong style={{ color: 'red' }}>Netflix</strong> and <strong style={{ color: '#58bae3' }}>Meta</strong>. I'm currently working a contract position as a swift developer, and about to start a masters in computer science! I also identify as non-binary, and go by she/her pronouns.
+                                                I'm a junior software engineer with previous back-end experience at <strong style={{ color: 'red' }}>Netflix</strong> and <strong style={{ color: '#58bae3' }}>Meta</strong>. I'm currently working at a SaaS start-up, and about to start a masters in computer science! I also identify as non-binary, and go by she/her pronouns.
                                             </p>
                                             <br/>
                                             <p className="text-slate-300">
